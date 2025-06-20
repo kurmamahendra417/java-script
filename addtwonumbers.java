5e86da7a2e3cebed05fe20bac0672107a467d2e8
kurmamahendra417 <mahendrakurmamahendra@gmail.com>
2025-06-20 14:25:04 +0530

    Delete addtwonumbers.java

--- addtwonumbers.java
+++
@@ -1,9 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int num1 = 16;   
-        int num2 = 17;   
-        int sum = num1+num2;
-
-        System.out.println("The sum is: " + sum);  
-    }
-}
